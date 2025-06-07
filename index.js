97c80dfd8dbe050f015a948160282e4694872e6c
Suprotik007 <csuprotik@gmail.com>
2025-06-08 01:17:44 +0600

    room availability checking by using get method

--- index.js
+++ index.js
@@ -1,80 +1,85 @@
-const express=require('express')
-const cors=require('cors')
-require('dotenv').config()
-const app =express()
-const port=process.env.PORT ||3000
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors())
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('cozy-rooms are roaming')
+app.get('/', (req, res) => {
+  res.send('cozy-rooms are roaming');
 });
-
-app.listen(port,()=>{
-        console.log(`book cozy rooms ${port}`);
-        
-    })
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.8n6fjbk.mongodb.net/?retryWrites=true&w=majority&appName=cluster0`;
 
-
-// featured rooms
-app.get('/topRooms',async(req,res)=>{
-    const roomsCollection = client.db('cozy-rooms').collection('rooms-collection');
-    const topRooms=await roomsCollection
-    .find({})
-      .limit(6)
-      .toArray();
-    res.json(topRooms);  
-})
-
-// allRooms
-app.get('/allRooms',async (req,res)=>{
-  const roomsCollection=client.db('cozy-rooms').collection('rooms-collection')
-  const allRooms=await roomsCollection
-  .find({})
-  .toArray()
-  res.json(allRooms)
-  console.log(allRooms);  
-})
-
-//roomDetails 
-app.get('/allRooms/:id',async(req,res)=>{
-  const id=req.params.id
-  const room=client.db('cozy-rooms').collection('rooms-collection')
-  const roomDetails=await room .findOne({ _id: new ObjectId(id) })
-  res.json(roomDetails)
-  
-})
-// bookingData
-app.post('/bookedRooms',async(req,res)=>{
-const bookedRooms = req.body;
-console.log(bookedRooms);
-const bookingDataCollection=client.db('cozy-rooms').collection('BookingData-collection')
-const result=await bookingDataCollection.insertOne(bookedRooms)
-res.send(result)
-
-    })
-
-    const client = new MongoClient(uri, {
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    const db = client.db('cozy-rooms');
+    const roomsCollection = db.collection('rooms-collection');
+    const bookingDataCollection = db.collection('BookingData-collection');
+
+    console.log('Connected to MongoDB!');
+
+    //featuredRooms
+    app.get('/topRooms', async (req, res) => {
+      const topRooms = await roomsCollection
+      .find({})
+      .limit(6)
+      .toArray();
+      res.json(topRooms);
+    });
+
+    //allRooms
+    app.get('/allRooms', async (req, res) => {
+      const allRooms = await roomsCollection
+      .find({})
+      .toArray();
+      res.json(allRooms);
+    });
+
+    //roomDetails
+    app.get('/allRooms/:id', async (req, res) => {
+      const id = req.params.id;
+      const roomDetails = await roomsCollection.findOne({ _id: new ObjectId(id) });
+      res.json(roomDetails);
+    });
+
+    // bookedRooms
+    app.post('/bookedRooms/:id', async (req, res) => {
+      const roomId = req.params.id;
+      const booking = {
+        roomId: roomId,
+        ...req.body
+      };
+      const result = await bookingDataCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    //bookChecking
+    app.get('/bookedRooms/:id', async (req, res) => {
+      const roomId = req.params.id;
+      const existingBooking = await bookingDataCollection.findOne({ roomId: roomId });
+      res.send({ isBooked: !!existingBooking });
+    });
+
+    app.listen(port, () => {
+      console.log(`Cozy Rooms server running on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
   }
 }
+
 run().catch(console.dir);
